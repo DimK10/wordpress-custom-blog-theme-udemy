8b0c35cc46c1a6ee23d8b72af199c208980a2496
DimK10 <james_k23@hotmail.gr>
2021-06-23 21:52:17 +0300

    Lesson 42 complete

--- functions.php
+++ functions.php
@@ -49,6 +49,14 @@ add_action('after_setup_theme', 'university_features');
 // is_main_query says that this query is not the result of a custom query
 function university_adjust_queries($query)
 {
+
+    if (!is_admin() and is_post_type_archive('program') and is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1); // all will be listed at once
+    }
+
+
     if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
         $today = date('Ymd');
 
