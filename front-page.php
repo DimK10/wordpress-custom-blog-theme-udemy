6487b10295732250c796b8331707d857e8909fb6
DimK10 <james_k23@hotmail.gr>
2021-06-23 20:41:23 +0300

    Lesson 39 complete

--- front-page.php
+++ front-page.php
@@ -18,29 +18,53 @@
                 <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
                 <?php
-                $homePageEvents = new WP_Query(array(
-                    'posts_per_page' => 2,
-                    'post_type' => 'event'
-                ));
+                    $today = date('Ymd');
 
-                while ($homePageEvents->have_posts()) {
-                    $homePageEvents->the_post(); ?>
-                    <div class="event-summary">
-                        <a class="event-summary__date t-center" href="#">
-                            <span class="event-summary__month">Mar</span>
-                            <span class="event-summary__day">25</span>
-                        </a>
-                        <div class="event-summary__content">
-                            <h5 class="event-summary__title headline headline--tiny">
-                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                            </h5>
-                            <p><?php if (has_excerpt()) {
-                                    echo get_the_excerpt();
-                                } else {
-                                    wp_trim_words(get_the_content(), 18);
-                                }?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                    // -1 returns all posts that meet the below conditions
+                    $homePageEvents = new WP_Query(array(
+                        'posts_per_page' => 2,
+                        'post_type' => 'event',
+                        'meta_key' => 'event_date',
+                        'orderby' => 'meta_value_num',
+                        'order' => 'ASC',
+                        'meta_query' => array(
+                            array(
+                                'key' => 'event_date',
+                                'compare' => '>=',
+                                'value' => $today,
+                                'type' => 'numeric'
+                            )
+                        )
+                    ));
+
+                    while ($homePageEvents->have_posts()) {
+                        $homePageEvents->the_post(); ?>
+                        <div class="event-summary">
+                            <a class="event-summary__date t-center" href="#">
+                                <span class="event-summary__month">
+                                    <?php
+                                        $eventDate = new DateTime(get_field('event_date'));
+                                        echo $eventDate->format('M');
+                                    ?>
+                                </span>
+                                <span class="event-summary__day">
+                                     <?php
+                                         $eventDate = new DateTime(get_field('event_date'));
+                                         echo $eventDate->format('d');
+                                     ?>
+                                </span>
+                            </a>
+                            <div class="event-summary__content">
+                                <h5 class="event-summary__title headline headline--tiny">
+                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                </h5>
+                                <p><?php if (has_excerpt()) {
+                                        echo get_the_excerpt();
+                                    } else {
+                                        wp_trim_words(get_the_content(), 18);
+                                    }?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                            </div>
                         </div>
-                    </div>
                 <?php }
 
                 ?>
