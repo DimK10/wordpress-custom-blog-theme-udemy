cbea34ec12bfee3ce4ef2ee6718b4780bc449887
DimK10 <james_k23@hotmail.gr>
2021-07-06 21:04:37 +0300

    Lesson 51 complete

--- functions.php
+++ functions.php
@@ -1,5 +1,37 @@
 <?php
 
+function pageBanner($args = NULL)
+{
+
+    if (!$args['title']) {
+        $args['title'] = get_the_title();
+    }
+
+    if (!$args['subtitle']) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+
+    if (!$args['photo']) {
+        if (get_field('page_banner_background_image') AND !is_archive() AND !is_home()) {
+            $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+        } else {
+            $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+        }
+    }
+
+    ?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+            <div class="page-banner__intro">
+                <p><?php echo $args['subtitle']; ?></p>
+            </div>
+        </div>
+    </div>
+<?php }
+
 // This is for loading css js
 function university_files()
 {
@@ -42,6 +74,7 @@ function university_features()
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape', 400, 260, true);
     add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');
@@ -66,7 +99,7 @@ function university_adjust_queries($query)
         $query->set('meta_key', 'event_date');
         $query->set('orderby', 'meta_value_num');
         $query->set('order', 'ASC');
-        $query->set('meta_query',  array(
+        $query->set('meta_query', array(
             array(
                 'key' => 'event_date',
                 'compare' => '>=',
--- single-program.php
+++ single-program.php
@@ -55,7 +55,7 @@ while (have_posts()) {
                 $relatedProfessors->the_post(); ?>
                 <li class="professor-card__list-item">
                     <a class="professor-card" href="<?php the_permalink(); ?>">
-                        <img class="professor-card__image" src="<?php the_post_thumbnail(); ?>">
+                        <img class="professor-card__image" src="<?php the_post_thumbnail('professorLandscape'); ?>">
                         <span class="professor-card__name"><?php the_title(); ?></span>
                     </a>
                 </li>
